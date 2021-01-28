c4c7409e5acf6e7d61d3d893fbe4590e8e37734d
Caroline Liu <17cliu@users.noreply.github.com>
2021-01-27 20:05:52 -0500

    Add form to get params from user + reorganize code

--- src/App.js
+++ src/App.js
@@ -1,114 +1,55 @@
-import { useEffect, useState } from 'react';
-// import Action from './components/Action';
-import EventsByDayOfWeek from './components/EventsByDayOfWeekChart';
-import EventsByTypeChart from './components/EventsByTypeChart';
-import DownloadLink from './components/DownloadLink';
-import Loader from './components/Loader';
-import Tapestry from './components/Tapestry';
-import Statistics from './components/Statistics';
-import fetchData from './mockApi';
-import {
-    floorDateToClosestSunday,
-    formatLongDate,
-    formatIsoDate,
-    generateDateStampsInRange
-} from './utils';
+// import { useEffect, useState } from 'react';
+// import Dashboard from './components/Dashboard';
 
 function App() {
-    const [result, setResult] = useState({ data: [], total: 0 });
-    const [numItemsFetched, setNumItemsFetched] = useState(0);
-    const [isLoading, setIsLoading] = useState(true);
-
-    // On app load, fetch all data
-    useEffect(() => {
-        fetchData().then(initialResponse => {
-            setResult(initialResponse);
-            setNumItemsFetched(initialResponse.data.length);
-            console.log('got first page', initialResponse);
-
-            if (!initialResponse.nextPage) {
-                // This is all the data there is!
-                setIsLoading(false);
-            } else {
-                // Fetch the remaining pages.
-
-                // Array(n) generates [0, 1, 2, ... n-1]. Because page numbers are
-                // 1-indexed, we generate `totalPages + 1` numbers, then slice off
-                // the 0. We also slice off the 1, because we already fetched the
-                // first page.
-                const pagesToFetch = [...Array(initialResponse.totalPages + 1).keys()].slice(2);
-                console.log('attempting to fetch more pages:', pagesToFetch);
-
-                const promises = pagesToFetch.map(async page => {
-                    // await new Promise(resolve => setTimeout(resolve, Math.random() * 10000));
-                    const data = await fetchData({ page });
-                    setNumItemsFetched(n => n + data.data.length);
-                    return data;
-                });
-
-                Promise.allSettled(promises).then(outcomes => {
-                    const newResult = outcomes.reduce((memo, outcome, i) => {
-                        if (outcome.status === 'fulfilled') {
-                            return {
-                                ...memo,
-                                nextPage: outcome.value.nextPage,
-                                data: memo.data.concat(outcome.value.data)
-                            };
-                        } else {
-                            console.log('Failed to fetch chunk of events!', i);
-                            return memo;
-                        }
-                    }, { ...initialResponse });
-
-                    setResult(newResult);
-                    setIsLoading(false);
-                });
-            }
-        });
-    }, []);
-
-    const { data, total } = result;
-
-    if (isLoading) {
-        return <Loader current={numItemsFetched} total={total} />;
-    }
-
-    const oldestDate = floorDateToClosestSunday(data[data.length - 1].created_at);
-    const newestDate = new Date(data[0].created_at);
-    const dates = generateDateStampsInRange(oldestDate, newestDate);
-
-    // Count events per day
-    const countsByDay = dates.reduce((memo, s) => ({ ...memo, [s]: 0 }), {});
-    data.forEach(o => {
-        const d = formatIsoDate(new Date(o.created_at));
-        countsByDay[d]++;
-    });
-
-    const tapestryCells = Object.keys(countsByDay).reverse().map(s => ({
-        label: s,
-        count: countsByDay[s]
-    }));
-
     return (
         <div>
             <h1>How spicy is your GitLab activity? 🔥</h1>
-            <p>
-                Showing <b>{data.length} events</b> from {
-                    formatLongDate(oldestDate)} to {formatLongDate(newestDate)}.
-            </p>
 
-            <Tapestry cells={tapestryCells} startDate={oldestDate} />
+            <form className="form">
+                <div className="form-row">
+                    <label for="host">
+                        Your GitLab domain
+                        ("gitlab.com", or "git.mycompany.com"
+                        if you're on a self-hosted instance of GitLab)
+                    </label>
+                    <input type="text"
+                        className="text-input"
+                        id="host"
+                        placeholder="git.transmissionmedia.ca"
+                    />
+                </div>
 
-            <div className="charts">
-                <EventsByDayOfWeek data={data} />
-                <EventsByTypeChart data={data} />
-                <Statistics data={data} startDate={oldestDate} endDate={newestDate} />
-            </div>
+                <div className="form-row">
+                    <label for="userId">
+                        Your numerical user ID
+                        (find this at User Settings &gt; Edit Profile)
+                    </label>
+                    <input
+                        type="text"
+                        className="text-input"
+                        id="userId"
+                        placeholder="4"
+                    />
+                </div>
 
-            {/* <ol className="activity-list">
-                {result.map(o => <Action key={o.id} {...o} />)}
-            </ol> */}
-            <DownloadLink data={result} />
+                <div className="form-row">
+                    <label for="accessToken">
+                        Your access token
+                        (generate one with `read_user` scope at User Settings &gt; Access Tokens)
+                    </label>
+                    <input
+                        type="text"
+                        className="text-input"
+                        id="accessToken"
+                        placeholder="YOUR_API_KEY"
+                    />
+                </div>
+
+                <input type="button" className="btn" value="Go!" />
+            </form>
+
+            {/* <Dashboard /> */}
         </div>
     );
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,115 @@
+import { useEffect, useState } from 'react';
+// import Action from './Action';
+import fetchData from '../mockApi';
+import {
+    floorDateToClosestSunday,
+    formatLongDate,
+    formatIsoDate,
+    generateDateStampsInRange
+} from '../utils';
+import EventsByDayOfWeek from './EventsByDayOfWeekChart';
+import EventsByTypeChart from './EventsByTypeChart';
+import DownloadLink from './DownloadLink';
+import Loader from './Loader';
+import Tapestry from './Tapestry';
+import Statistics from './Statistics';
+
+function Dashboard() {
+    const [result, setResult] = useState({ data: [], total: 0 });
+    const [numItemsFetched, setNumItemsFetched] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+
+    // On app load, fetch all data
+    useEffect(() => {
+        fetchData().then(initialResponse => {
+            setResult(initialResponse);
+            setNumItemsFetched(initialResponse.data.length);
+            console.log('got first page', initialResponse);
+
+            if (!initialResponse.nextPage) {
+                // This is all the data there is!
+                setIsLoading(false);
+            } else {
+                // Fetch the remaining pages.
+
+                // Array(n) generates [0, 1, 2, ... n-1]. Because page numbers are
+                // 1-indexed, we generate `totalPages + 1` numbers, then slice off
+                // the 0. We also slice off the 1, because we already fetched the
+                // first page.
+                const pagesToFetch = [...Array(initialResponse.totalPages + 1).keys()].slice(2);
+                console.log('attempting to fetch more pages:', pagesToFetch);
+
+                const promises = pagesToFetch.map(async page => {
+                    // await new Promise(resolve => setTimeout(resolve, Math.random() * 10000));
+                    const data = await fetchData({ page });
+                    setNumItemsFetched(n => n + data.data.length);
+                    return data;
+                });
+
+                Promise.allSettled(promises).then(outcomes => {
+                    const newResult = outcomes.reduce((memo, outcome, i) => {
+                        if (outcome.status === 'fulfilled') {
+                            return {
+                                ...memo,
+                                nextPage: outcome.value.nextPage,
+                                data: memo.data.concat(outcome.value.data)
+                            };
+                        } else {
+                            console.log('Failed to fetch chunk of events!', i);
+                            return memo;
+                        }
+                    }, { ...initialResponse });
+
+                    setResult(newResult);
+                    setIsLoading(false);
+                });
+            }
+        });
+    }, []);
+
+    const { data, total } = result;
+
+    if (isLoading) {
+        return <Loader current={numItemsFetched} total={total} />;
+    }
+
+    const oldestDate = floorDateToClosestSunday(data[data.length - 1].created_at);
+    const newestDate = new Date(data[0].created_at);
+    const dates = generateDateStampsInRange(oldestDate, newestDate);
+
+    // Count events per day
+    const countsByDay = dates.reduce((memo, s) => ({ ...memo, [s]: 0 }), {});
+    data.forEach(o => {
+        const d = formatIsoDate(new Date(o.created_at));
+        countsByDay[d]++;
+    });
+
+    const tapestryCells = Object.keys(countsByDay).reverse().map(s => ({
+        label: s,
+        count: countsByDay[s]
+    }));
+
+    return (
+        <div>
+            <p>
+                Showing <b>{data.length} events</b> from {
+                    formatLongDate(oldestDate)} to {formatLongDate(newestDate)}.
+            </p>
+
+            <Tapestry cells={tapestryCells} startDate={oldestDate} />
+
+            <div className="charts">
+                <EventsByDayOfWeek data={data} />
+                <EventsByTypeChart data={data} />
+                <Statistics data={data} startDate={oldestDate} endDate={newestDate} />
+            </div>
+
+            {/* <ol className="activity-list">
+                {result.map(o => <Action key={o.id} {...o} />)}
+            </ol> */}
+            <DownloadLink data={result} />
+        </div>
+    );
+}
+
+export default Dashboard;
